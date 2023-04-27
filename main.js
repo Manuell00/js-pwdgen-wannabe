8e1c72f21b3424e47f30ed29e9b5d2e64ddf4cb8
Manuell00 <manuell.caselli@gmail.com>
2023-04-27 10:06:30 +0200

    Aggiunta media query per immagine > 1500px

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-// REQUEST : Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
-// Infine scrivi sulla pagina nomecognomecolorepreferito21
+// // REQUEST : Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-// VARIABILI
-let nome, cognome, colore, numero, password;
+// // VARIABILI
+// let nome, cognome, colore, numero, password;
 
-// Assegno i valori alle variabili e stampo a schermo
-nome = prompt ("Inserisci il tuo nome")
-document.getElementById("mio_nome").innerHTML = nome
+// // Assegno i valori alle variabili e stampo a schermo
+// nome = prompt ("Inserisci il tuo nome")
+// document.getElementById("mio_nome").innerHTML = nome
 
-cognome = prompt ("Inserisci il tuo cognome")
-document.getElementById("mio_cognome").innerHTML = cognome
+// cognome = prompt ("Inserisci il tuo cognome")
+// document.getElementById("mio_cognome").innerHTML = cognome
 
-colore = prompt ("Inserisci il tuo colore preferito")
-document.getElementById("mio_colore").innerHTML = colore
+// colore = prompt ("Inserisci il tuo colore preferito")
+// document.getElementById("mio_colore").innerHTML = colore
 
-numero = prompt ("Inserisci il tuo numero preferito")
-document.getElementById("mio_numero").innerHTML = numero
+// numero = prompt ("Inserisci il tuo numero preferito")
+// document.getElementById("mio_numero").innerHTML = numero
 
-// Compongo e assegno la rispttiva stringa alla variabile password
-password = nome+cognome+colore+numero
+// // Compongo e assegno la rispttiva stringa alla variabile password
+// password = nome+cognome+colore+numero
 
-// Stampo a schermo la password
-document.getElementById("mia_password").innerHTML = password
+// // Stampo a schermo la password
+// document.getElementById("mia_password").innerHTML = password
 
-// Creo la funzione per copiare il testo
-function copyText() {
+// // Creo la funzione per copiare il testo
+// function copyText() {
       
-    /* Copy text into line password */
-    navigator.clipboard.writeText
-        (password);
-}
+//     /* Copy text into line password */
+//     navigator.clipboard.writeText
+//         (password);
+// }
+
